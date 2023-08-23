f6ebe8334f5c012b8508550adcee8cfa41f7a84d
gabri <gabri@DESKTOP-G23QIT1>
2023-08-22 19:29:21 -0600

    Agregar archivos de proyecto.

---
+++ Descuento.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Ventas
+{
+    partial class Descuento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.btnRecibo = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCompra = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDescuento = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(105, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE VENTAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DIGITE A CANTIDAD DE PRODUCTOS A LLEVAR :";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(44, 128);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(257, 20);
+            this.txtCantidad.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(227, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "INTRODUZCA EL VALOR DEL PRODUCTO :";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(45, 208);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(256, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // btnRecibo
+            // 
+            this.btnRecibo.Location = new System.Drawing.Point(45, 266);
+            this.btnRecibo.Name = "btnRecibo";
+            this.btnRecibo.Size = new System.Drawing.Size(126, 65);
+            this.btnRecibo.TabIndex = 5;
+            this.btnRecibo.Text = "RECIBO";
+            this.btnRecibo.UseVisualStyleBackColor = true;
+            this.btnRecibo.Click += new System.EventHandler(this.btnRecibo_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(219, 266);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(126, 65);
+            this.btnNuevo.TabIndex = 6;
+            this.btnNuevo.Text = "NUEVA COMPRA";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 357);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "IMPORTE DE LA COMPRA:";
+            // 
+            // txtCompra
+            // 
+            this.txtCompra.Location = new System.Drawing.Point(45, 384);
+            this.txtCompra.Name = "txtCompra";
+            this.txtCompra.Size = new System.Drawing.Size(142, 20);
+            this.txtCompra.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 357);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "IMPORTE DEL DESCUENTO:";
+            // 
+            // txtDescuento
+            // 
+            this.txtDescuento.Location = new System.Drawing.Point(219, 384);
+            this.txtDescuento.Name = "txtDescuento";
+            this.txtDescuento.Size = new System.Drawing.Size(150, 20);
+            this.txtDescuento.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(156, 431);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "TOTAL A PAGAR:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(101, 456);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(201, 20);
+            this.txtTotal.TabIndex = 12;
+            // 
+            // Descuento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 537);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDescuento);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCompra);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnRecibo);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Descuento";
+            this.Text = "Descuento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Button btnRecibo;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCompra;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDescuento;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTotal;
+    }
+}
---
+++ Descuento.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ventas
+{
+    public partial class Descuento : Form
+    {
+        public Descuento()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRecibo_Click(object sender, EventArgs e)
+        {
+            double Cantidad, Valor, Compra, Descuento, Total;
+
+            Cantidad = double.Parse(txtCantidad.Text);
+            Valor = double.Parse(txtValor.Text);
+
+            Compra = (Cantidad * Valor);
+
+            txtCompra.Text = Compra.ToString();
+
+            Descuento = (Compra * 0.11);
+
+            txtDescuento.Text = Descuento.ToString();
+
+            Total = (Compra - Descuento);
+
+            txtTotal.Text = Total.ToString();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtCantidad.Clear();
+            txtValor.Clear();
+            txtCompra.Clear();
+            txtDescuento.Clear();
+            txtTotal.Clear();
+        }
+    }
+}
